9839bc180aac798c82e15bf3bd1a55bf044b1e14
NEETHA <neethababukply@gmail.com>
2025-12-09 11:43:17 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:msg_app/firebase_options.dart';
 import 'package:msg_app/pages/login_or_register_page.dart';
 import 'package:msg_app/themes/light_mode.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
